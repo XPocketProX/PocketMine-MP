ee5d280a332aadabffb1031baabe939c2e2cb6f9
KnosTx <135945845+KnosTx@users.noreply.github.com>
2024-10-17 14:49:28 +0700

    fix cs

--- src/block/Observer.php
+++ src/block/Observer.php
@@ -1,71 +1,84 @@
 <?php
 
+/*
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
+ *
+ *
+ */
+
 namespace pocketmine\block;
 
 use pocketmine\block\utils\AnyFacingTrait;
 use pocketmine\block\utils\PoweredByRedstoneTrait;
 use pocketmine\data\runtime\RuntimeDataDescriber;
 use pocketmine\item\Item;
+use pocketmine\math\AxisAlignedBB;
 use pocketmine\math\Facing;
 use pocketmine\math\Vector3;
 use pocketmine\player\Player;
 use pocketmine\world\BlockTransaction;
 use pocketmine\world\World;
-use pocketmine\math\AxisAlignedBB;
 
 class Observer extends Opaque {
-  use AnyFacingTrait;
-  use PoweredByRedstoneTrait;
+    use AnyFacingTrait;
+    use PoweredByRedstoneTrait;
 
-  protected function describeBlockOnlyState(RuntimeDataDescriber $w) : void{
-    $w->facing($this->facing);
-  }
+	protected function describeBlockOnlyState(RuntimeDataDescriber $w) : void{
+		$w->facing($this->facing);
+	}
 
-  public function place(BlockTransaction $tx, Item $item, Block $blockReplace, Block $blockClicked, int $face, Vector3 $clickVector, ?Player $player = null) : bool{
-    if($player !== null){
-      if(abs($player->getPosition()->x - $this->position->x) < 2 && abs($player->getPosition()->z - $this->position->z) < 2){
-        $y = $player->getEyePos()->y;
-        if($y - $this->position->y > 2){
-                    $this->facing = Facing::DOWN;
-                }elseif($this->position->y - $y > 0){
-                    $this->facing = Facing::UP;
-                }else{
-                    $this->facing = $player->getHorizontalFacing();
-                }
-            }else{
-                $this->facing = $player->getHorizontalFacing();
-            }
-        }
+	public function place(BlockTransaction $tx, Item $item, Block $blockReplace, Block $blockClicked, int $face, Vector3 $clickVector, ?Player $player = null) : bool{
+		if($player !== null){
+			if(abs($player->getPosition()->x - $this->position->x) < 2 && abs($player->getPosition()->z - $this->position->z) < 2){
+				$y = $player->getEyePos()->y;
 
-        return parent::place($tx, $item, $blockReplace, $blockClicked, $face, $clickVector, $player);
-    }
+				if($y - $this->position->y > 2){
+					$this->facing = Facing::DOWN;
+				}elseif($this->position->y - $y > 0){
+					$this->facing = Facing::UP;
+				}else{
+					$this->facing = $player->getHorizontalFacing();
+				}
+			}else{
+				$this->facing = $player->getHorizontalFacing();
+			}
+		}
 
-    public function onUpdate(int $type) : bool{
-        if($type === World::BLOCK_UPDATE_NORMAL || $type === World::BLOCK_UPDATE_REDSTONE){
-            $this->activate();
-        }
-        return parent::onUpdate($type);
-    }
+		return parent::place($tx, $item, $blockReplace, $blockClicked, $face, $clickVector, $player);
+	}
 
-    public function activate() : void{
-        if(!$this->isPowered()){
-            $this->setPowered(true);
+	public function onNearbyBlockChange() : void{
+		$this->activate();
+	}
 
-            $facingOpposite = Facing::opposite($this->facing);
-            $neighborBlockPos = $this->position->getSide($facingOpposite);
-            $this->getWorld()->scheduleDelayedBlockUpdate($this, 2);
+	public function activate() : void{
+		if(!$this->isPowered()){
+			$this->setPowered(true);
+			$facingOpposite = Facing::opposite($this->facing);
+			$neighborBlockPos = $this->position->getSide($facingOpposite);
+			$world = $this->position->getWorld();
+			$world->scheduleDelayedBlockUpdate($this, 2);
+			$world->setBlock($neighborBlockPos, $world->getBlock($neighborBlockPos)->setPowered(true));
+			$world->scheduleDelayedTask(function() use ($neighborBlockPos, $world) {
+				$this->setPowered(false);
+				$world->setBlock($neighborBlockPos, $world->getBlock($neighborBlockPos)->setPowered(false));
+			}, 2);
+		}
+	}
 
-            $this->getWorld()->setBlock($neighborBlockPos, $this->getWorld()->getBlock($neighborBlockPos)->setPowered(true));
-
-            $this->getWorld()->scheduleDelayedTask(function() use ($neighborBlockPos) {
-                $this->setPowered(false);
-                $this->getWorld()->setBlock($neighborBlockPos, $this->getWorld()->getBlock($neighborBlockPos)->setPowered(false));
-            }, 2);
-        }
-    }
-
-    protected function recalculateCollisionBoxes() : array{
-        return [AxisAlignedBB::one()->stretch($this->getFacingVector())];
-    }
-
+	protected function recalculateCollisionBoxes() : array{
+		return [AxisAlignedBB::one()->stretch($this->getWidth(), $this->getHeight())];
+	}
 }
